0469f920e0e8809110ac77b97336d88c4546238b
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-11-04 10:33:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Heading, Text, Center, Button } from '@chakra-ui/core'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Center>
+        <Heading>Web3 demo 1</Heading>
+      </Center>
+    </>
+  )
 }
 
-export default App;
+export default App
